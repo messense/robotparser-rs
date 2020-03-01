503d351b2e76f4c980287667722edfea7e1167d7
Laurent Arnoud <laurent@spkdev.net>
2020-03-01 18:49:39 +0100

    Add test when url is invalid and panic

--- tests/test_reqwest_async.rs
+++ tests/test_reqwest_async.rs
@@ -3,6 +3,7 @@ use robotparser::service::RobotsTxtService;
 use reqwest::Client;
 use url::Url;
 use tokio::runtime::Runtime;
+use url::{Host, Origin};
 
 #[test]
 fn test_reqwest_async() {
@@ -13,4 +14,15 @@ fn test_reqwest_async() {
     let robots_txt = robots_txt_response.unwrap().get_result();
     let fetch_url = Url::parse("https://www.python.org/robots.txt").unwrap();
     assert!(robots_txt.can_fetch("*", &fetch_url));
+    let fetch_url = Url::parse("http://www.python.org/webstats/").unwrap();
+    assert!(!robots_txt.can_fetch("*", &fetch_url));
+}
+
+#[test]
+#[should_panic]
+fn test_reqwest_blocking_panic_url() {
+    let client = Client::new();
+    let host = Host::Domain("python.org::".into());
+    let origin = Origin::Tuple("https".into(), host, 80);
+    client.fetch_robots_txt(origin);
 }
